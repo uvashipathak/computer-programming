bfa87a1f82da6ca0b1e0b95514b63b9af9197d4a
urvashi pathak <urvashi.pathak_cs.h24@gla.ac.in>
2024-11-28 23:51:21 +0530

    Add files via upload

---
+++ coding-assignment/dict/dict_q06.py
@@ -0,0 +1,18 @@
+"""
+@geeksforgeeks
+Python program to find the size of a Dictionary
+"""
+
+def dictionary_size(dictionary: dict) -> int:
+    """
+    Parameters:
+    ----------
+    dictionary : dict
+        The dictionary whose size is to be calculated.
+
+    Returns:
+    -------
+    int
+        The number of key-value pairs in the dictionary
+    """
+    return len(dictionary)
