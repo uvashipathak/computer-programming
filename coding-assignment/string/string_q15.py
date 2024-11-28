6cfc6072c4aa36125698f63d2da7988873ce09cd
urvashi pathak <urvashi.pathak_cs.h24@gla.ac.in>
2024-11-29 00:04:14 +0530

    Add files via upload

---
+++ coding-assignment/string/string_q15.py
@@ -0,0 +1,7 @@
+# Check if a String Contains Only Digits
+def is_numeric(s):
+    return s.isdigit()
+
+# Example
+print(is_numeric("12345"))  # Output: True
+print(is_numeric("123a45"))  # Output: False
