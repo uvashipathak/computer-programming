80615921e237481d0203ce8132060c333ec9c719
urvashi pathak <urvashi.pathak_cs.h24@gla.ac.in>
2024-11-29 00:02:48 +0530

    Add files via upload

---
+++ coding-assignment/string/string_q14.py
@@ -0,0 +1,6 @@
+#Replace a Substring
+def replace_substring(s, old, new):
+    return s.replace(old, new)
+
+# Example
+print(replace_substring("hello world", "world", "Python"))  # Output: "hello Python"
