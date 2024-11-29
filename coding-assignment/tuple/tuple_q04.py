43d2921c7811b383512b85e20beee6910fc87d7f
urvashi pathak <urvashi.pathak_cs.h24@gla.ac.in>
2024-11-29 22:11:12 +0530

    Add files via upload

---
+++ coding-assignment/tuple/tuple_q04.py
@@ -0,0 +1,8 @@
+def reverse_tuple(tpl):
+    """
+    Problem 4: Reverse a tuple.
+    Input: tpl = (1, 2, 3, 4)
+    Output: (4, 3, 2, 1)
+    Explanation: The tuple is reversed.
+    """
+    return tpl[::-1]
