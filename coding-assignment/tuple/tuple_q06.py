87fd2ec34026b8dbaf9a860f6c672cad8794d45d
urvashi pathak <urvashi.pathak_cs.h24@gla.ac.in>
2024-11-29 22:09:26 +0530

    Add files via upload

---
+++ coding-assignment/tuple/tuple_q06.py
@@ -0,0 +1,8 @@
+def count_occurrences_of_element(tpl, element):
+    """
+    Problem 6: Count the occurrences of an element in a tuple.
+    Input: tpl = (1, 2, 2, 3, 4, 2), element = 2
+    Output: 3
+    Explanation: The element 2 appears 3 times in the tuple.
+    """
+    return tpl.count(element)
