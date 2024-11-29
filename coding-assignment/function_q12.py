261eacfcff707cf97d1eb47cc3b622b227fed5ef
urvashi pathak <urvashi.pathak_cs.h24@gla.ac.in>
2024-11-29 10:15:18 +0530

    Add files via upload

---
+++ coding-assignment/function_q12.py
@@ -0,0 +1,8 @@
+def count_digits(n):
+    """
+    Problem 12: Write a function to count the number of digits in a number.
+    Input: n = 12345
+    Output: 5
+    Explanation: There are 5 digits in the number 12345.
+    """
+    return len(str(n))
